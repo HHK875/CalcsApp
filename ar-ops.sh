b985aae7842120878014b9053dca85cd286d21a4
HHK875 <HHK875@gmail.com>
2022-10-23 05:14:53 +0000

    mul-calcs added to ar-ops.sh

--- ar-ops.sh
+++ ar-ops.sh
@@ -10,3 +10,6 @@ echo "Sum of $num1 and $num2 is: $sum"
 sub=0
 sub=$(( $sub + $num1 - $num2 ))
 echo "Difference of $num1 and $num2 is: $sub"
+mul=1
+mul=$(( $mul * $num1 * $num2 ))
+echo "Product of $num1 and $num2 is: $mul"
