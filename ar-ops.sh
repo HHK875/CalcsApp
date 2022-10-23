0500f8ad5834eb4baac74b1de45602b25602989a
HHK875 <HHK875@gmail.com>
2022-10-23 05:42:08 +0000

    Changes are completed

--- ar-ops.sh
+++ ar-ops.sh
@@ -6,10 +6,13 @@ echo "Enter the second number"
 read num2
 sum=0
 sum=$(( $sum + $num1 + $num2 ))
-echo "Sum of $num1 and $num2 is: $sum"
 sub=0
 sub=$(( $sub + $num1 - $num2 ))
-echo "Difference of $num1 and $num2 is: $sub"
 mul=1
 mul=$(( $mul * $num1 * $num2 ))
-echo "Product of $num1 and $num2 is: $mul"
+echo "----------------Summary of Arithematic Operattions:----------------"
+echo "                Sum of $num1 and $num2 is: $sum                    "     
+echo "                Difference of $num1 and $num2 is: $sub             "
+echo "                Product of $num1 and $num2 is: $mul                "
+echo "----------------  <END>  ------------------------------------------"
+
